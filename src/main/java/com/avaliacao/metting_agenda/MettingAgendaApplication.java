523fd06dff646019fb76c84c12c4e2f732d431be
Jean <32180506+jeanyamada@users.noreply.github.com>
2021-11-20 14:36:52 -0300

    first commit

---
+++ src/main/java/com/avaliacao/metting_agenda/MettingAgendaApplication.java
@@ -0,0 +1,13 @@
+package com.avaliacao.metting_agenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MettingAgendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MettingAgendaApplication.class, args);
+	}
+
+}
